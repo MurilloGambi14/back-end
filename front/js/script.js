1483569e2ebd1ec5c6bc2630aed8d4ac096cb3ad
MurilloGambi14 <24.10088-5@maua.br>
2024-10-18 09:12:34 -0300

    até estrutura para novo filme

--- front/js/script.js
+++ front/js/script.js
@@ -6,8 +6,23 @@ async function obterFilmes() {
     
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes = (await axios.get(URLcompleta)).data
+    let tabela = document.querySelector('.filmes')
+    let corpotabela = tabela.getElementsByTagName('tbody')[0]
 
-    
+    for (let filme of filmes) {
+      let linha = corpotabela.insertRow(0)
+      let celulatitulo = linha.insertCell(0)
+      let celulasinopse = linha.insertCell(1)
+      celulatitulo.innerHTML = filme.titulo
+      celulasinopse.innerHTML = filme.sinopse
+    }
     
     console.log(filmes)
 }
+
+
+// async quer dizer que pode demora um pouco para executar
+
+async function cadastrarfilme() {
+    console.log('CADASTRO')
+} 
